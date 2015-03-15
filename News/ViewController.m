243c78dcd1729fe9e8e1d57c1e534f44bc852278
KASEY MOFFAT <kaseymoffat@KASEYs-MacBook-Pro.local>
2015-03-14 23:31:18 -0700

    Initial Commit

---
+++ News/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  News
+//
+//  Created by KASEY MOFFAT on 3/14/15.
+//  Copyright (c) 2015 KASEY MOFFAT. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ News/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  News
+//
+//  Created by KASEY MOFFAT on 3/14/15.
+//  Copyright (c) 2015 KASEY MOFFAT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
